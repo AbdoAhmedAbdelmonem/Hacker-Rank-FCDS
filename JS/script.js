3c939f0ec400fe45e24eedcb99ae254152b56c8e
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-01-30 21:22:24 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -64,3 +64,16 @@ $('.team-members').slick({
                 slidesToScroll: 1,
             }
         }]});
+const main = document.querySelector('.main');
+const about = document.querySelector('.popo');
+const popup2 = document.querySelector('.popup2');
+const ok = document.querySelector('.info-ok-btn');
+
+about.onclick=()=>{
+    popup2.classList.add('active');
+    main.classList.add('active');
+};
+ok.onclick=()=>{
+    popup2.classList.remove('active');
+    main.classList.remove('active');
+};
